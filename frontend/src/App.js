f02a7d3ffb84e0df9fcc8c9fc50d8a3d9dd7a0b3
Skylar Shelton <skylarshelton@gmail.com>
2025-01-08 13:18:44 -0800

    test new computer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <p>Welcome To ProShop This is a test for github</p>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
